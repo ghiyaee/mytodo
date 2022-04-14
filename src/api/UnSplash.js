e4f9f4d5161a40180b5c8571150704bab8fba0e6
ghiyaee <86221396+ghiyaee@users.noreply.github.com>
2022-04-14 14:49:52 +0430

    update

--- src/api/UnSplash.js
+++ src/api/UnSplash.js
@@ -2,6 +2,6 @@ import axios from "axios";
 export default axios.create({
     baseURL: 'https://api.unsplash.com',
     headers: {
-         Authorization: 'Client-ID 35JyjzuaPSD0ZjD1gSA-g6eicsChTCV_4s8njbKaWR8'
+        Authorization: 'Client-ID 35JyjzuaPSD0ZjD1gSA-g6eicsChTCV_4s8njbKaWR8'
     }
 })
